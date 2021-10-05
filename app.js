2023d61701673692de20a8e6f0428a8205c8e300
Mariusz Szymanski <mariusz@melonio.pl>
2021-10-05 13:31:15 +0200

    add vue link

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn VUE'
+            courseGoal: 'Finish the course and learn VUE',
+            vueLink: 'https://vuejs.org/'
         }
     }
 });
